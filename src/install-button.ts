bcdcac1eab9f07d9b1e3099fa59ddfb667229516
Paulus Schoutsen <paulus@home-assistant.io>
2021-06-15 13:34:24 -0700

    Reverse guard for serial allowed (#19)

--- src/install-button.ts
+++ src/install-button.ts
@@ -84,9 +84,9 @@ export class InstallButton extends HTMLElement {
 
     if (!InstallButton.isSupported || !InstallButton.isAllowed) {
       this.toggleAttribute("install-unsupported", true);
-      this.renderRoot.innerHTML = !InstallButton.isSupported
-        ? "<slot name='unsupported'>Your browser does not support installing things on ESP devices. Use Google Chrome or Microsoft Edge.</slot>"
-        : "<slot name='not-allowed'>You can only install ESP devices on HTTPS websites or on the localhost.</slot>";
+      this.renderRoot.innerHTML = !InstallButton.isAllowed
+        ? "<slot name='not-allowed'>You can only install ESP devices on HTTPS websites or on the localhost.</slot>"
+        : "<slot name='unsupported'>Your browser does not support installing things on ESP devices. Use Google Chrome or Microsoft Edge.</slot>";
       return;
     }
 
