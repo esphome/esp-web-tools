deef684c169a65799eda305b619dc3130ac0af09
Paulus Schoutsen <balloob@gmail.com>
2022-04-13 13:46:45 -0700

    Make input select/textfield work better when bad REM defined

--- src/components/ewt-select.ts
+++ src/components/ewt-select.ts
@@ -1,5 +1,6 @@
 import { SelectBase } from "@material/mwc-select/mwc-select-base";
 import { styles } from "@material/mwc-select/mwc-select.css";
+import { css } from "lit";
 
 declare global {
   interface HTMLElementTagNameMap {
@@ -8,7 +9,15 @@ declare global {
 }
 
 export class EwtSelect extends SelectBase {
-  static override styles = [styles];
+  static override styles = [
+    styles,
+    // rem -> em conversion
+    css`
+      .mdc-floating-label {
+        line-height: 1.15em;
+      }
+    `,
+  ];
 }
 
 customElements.define("ewt-select", EwtSelect);
