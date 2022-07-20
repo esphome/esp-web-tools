1a8bee6604f8d4d9deb6f58e4b87a3ed77e8ef6d
Paulus Schoutsen <balloob@gmail.com>
2022-07-20 00:00:46 -0700

    Add babel to rewrite class properties (#271)

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import json from "@rollup/plugin-json";
 import { terser } from "rollup-plugin-terser";
+import { babel } from "@rollup/plugin-babel";
 
 const config = {
   input: "dist/install-button.js",
@@ -10,7 +11,14 @@ const config = {
   },
   external: ["https://www.improv-wifi.com/sdk-js/launch-button.js"],
   preserveEntrySignatures: false,
-  plugins: [nodeResolve(), json()],
+  plugins: [
+    nodeResolve(),
+    babel({
+      babelHelpers: "bundled",
+      plugins: ["@babel/plugin-proposal-class-properties"],
+    }),
+    json(),
+  ],
 };
 
 if (process.env.NODE_ENV === "production") {
